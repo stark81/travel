20cd4f412e8084baee257186547a11b21943771a
stark81 <stark81@qq.com>
2018-12-17 16:12:39 +0800

    create class RegisterForm

--- app/home/forms.py
+++ app/home/forms.py
@@ -2,3 +2,19 @@
 from flask_wtf import FlaskForm,
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Email,EqualTo
+
+class RegisterForm(FlaskForm):
+    username = StringField(
+        label="用户名",
+        validators=[
+            DataRequired("用户名不能为空!"),
+        ],
+        render_kw=[
+            "placeholder":"请输入用户名"
+        ]      
+    )
+    upwd = PasswordField(
+        label="密码",
+        validators=[],
+        render_kw=[]
+    )
